49b6868cb1c06c79447bda10bc5afaf57a248e22
damsalem <dani@theamsalems.com>
2018-11-20 11:29:03 -0800

    Added Car Insurance program

---
+++ BooleanCarInsurance/BooleanCarInsurance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanCarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Write \"True\" or \"False\".");
+            /////This works
+            bool dui = Boolean.Parse(Console.ReadLine());
+
+            /////This also works
+            //string duiInput = Console.ReadLine();
+            //bool dui = Convert.ToBoolean(duiInput);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string numberTickets = Console.ReadLine();
+            int numTickets = Int32.Parse(numberTickets);
+
+            bool qualified = (age >= 15 && (dui == false) && numTickets <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////AND operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            /////OR operator
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            /////TRUE operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            /////NOT TRUE operator
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            /////XOR operator, works only if one is true
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            /////Combine operators for more complex logic
+            Console.WriteLine(true && true && true && false);
+            Console.WriteLine(true && true && true || false);
+
+            Console.ReadLine();
+        }
+    }
+}
