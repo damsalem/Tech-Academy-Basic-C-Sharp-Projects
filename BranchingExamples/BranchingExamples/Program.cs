0f0447392e1d8e7d8286d7de217658c6847317fe
damsalem <dani@theamsalems.com>
2018-11-20 12:02:48 -0800

    Testing out branches

---
+++ BranchingExamples/BranchingExamples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BranchingExamples
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "Erik";
+            if (name == "Jesse")
+            {
+                Console.WriteLine("Your name is Jesse");
+            }
+            else if (name == "Brett")
+            {
+                Console.WriteLine("Your name is Brett");
+            }
+            else if (name == "Adam")
+            {
+                Console.WriteLine("Your name is Adam");
+            }
+            else
+                Console.WriteLine("I don't know you");
+            Console.ReadLine();
+        }
+        
+    }
+}
