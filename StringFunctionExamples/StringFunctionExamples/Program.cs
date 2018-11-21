724932fc6ba475d7abdfa48e85f028776c5aef4d
damsalem <dani@theamsalems.com>
2018-11-21 11:18:31 -0800

    Finished the string function examples

---
+++ StringFunctionExamples/StringFunctionExamples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "The man said \"Hello\" to me.";
+            string newLine = "Here is one line. \n And this is a new line.";
+            string tab = "\t Hello from a tabbed new line.";
+            string fileName1 = "C:\\Desktop\\New Folder";
+            string fileName2 = @"C:\Desktop\New Folder2";
+            
+            //check for true/false of string inclusions
+            bool trueOrFalse = quote.Contains("said");
+            trueOrFalse = quote.EndsWith("me");
+
+            //character length
+            int length = quote.Length;
+
+            //convert strings to case text
+            quote = quote.ToUpper();
+            quote = quote.ToLower();
+
+            //Console.WriteLine(quote);
+            //Console.WriteLine(newLine);
+            //Console.WriteLine(tab);
+            //Console.WriteLine(fileName1);
+            //Console.WriteLine(fileName2);
+            //Console.WriteLine(length);
+
+            //String builder is a dynamic object that acts as a buffer 
+            //to expand/shrink strings without a loss of performance
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Dani");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringPrograms/StringPrograms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press [Enter] to display 3 concatenated strings within ||");
+            Console.ReadLine();
+
+            string fName = "Dani";
+            string lName = "Amsalem";
+            string nationality = "American";
+
+            Console.WriteLine("My name is |" + fName + "| |" + lName + "| and I am an |" + nationality + "|");
+            Console.WriteLine("Next, you will see a string of words. Press [Enter] to continue.");
+            Console.ReadLine();
+
+            string favFood = "My favorite food is a Korean dish called \"Gopchang Bokkum\"";
+            Console.WriteLine(favFood);
+            Console.WriteLine("Press [Enter] to display the same string all in uppercase");
+            Console.ReadLine();
+
+            favFood = favFood.ToUpper();
+            Console.WriteLine(favFood);
+            Console.WriteLine("Next, you will see Stringbuilder build a paragraph of text, once sentence at a time.");
+            Console.WriteLine("Press [Enter] to continue;");
+            Console.ReadLine();
+
+
+        }
+    }
+}
