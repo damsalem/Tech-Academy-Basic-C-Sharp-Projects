2314ad75b16f4f9b601ccb4a4f8e64d357d97228
damsalem <dani@theamsalems.com>
2018-11-20 21:45:43 -0800

    Created a boolean WHILE and boolean DO WHILE program

---
+++ BooleanWhileandDoWhile/BooleanWhileandDoWhile/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanWhileandDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////
+            ///WHILE loop
+            /////
+            Console.WriteLine("Try to guess my favorite color");
+            string color = Console.ReadLine();
+            bool guess = color == "empty";
+
+            while (!guess)
+            {
+                switch (color)
+                {
+                    case "silver":
+                        Console.WriteLine("Ooo shiny! You're getting closer! Try again.");
+                        color = Console.ReadLine();
+                        break;
+
+                    case "white":
+                        Console.WriteLine("Bleh. Boring! Try again.");
+                        color = Console.ReadLine();
+                        break;
+
+                    case "blue":
+                        Console.WriteLine("Boring! Try again.");
+                        color = Console.ReadLine();
+                        break;
+
+                    case "black":
+                        Console.WriteLine("That's too dark. Try again.");
+                        color = Console.ReadLine();
+                        break;
+
+                    case "gold":
+                        Console.WriteLine("Wow! That's it!");
+                        guess = true;
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Hmmm. Nope! Try again.");
+                        color = Console.ReadLine();
+                        break;
+                }
+            }
+
+            /////
+            ///DO WHILE LOOP
+            ///
+            Console.WriteLine("What is your favorite city abbreviation?");
+            string city = Console.ReadLine();
+            bool aGuess = city == "NYC";
+
+            do
+            {
+                switch (city.ToLower())
+                {
+                    case "nyc":
+                        Console.WriteLine("Hey, that's my favorite city too!");
+                        aGuess = true;
+                        Console.ReadLine();
+                        break;
+
+                    case "pdx":
+                        Console.WriteLine("Yeah, that one's okay.");
+                        Console.WriteLine("Try again.");
+                        city = Console.ReadLine();
+                        break;
+
+                    case "atl":
+                        Console.WriteLine("I guess, it's in a lot of songs.");
+                        Console.WriteLine("Try again.");
+                        city = Console.ReadLine();
+                        break;
+
+                    case "sfo":
+                        Console.WriteLine("Sure, kind of like UFO");
+                        Console.WriteLine("Try again.");
+                        city = Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Meh...");
+                        Console.WriteLine("Try again.");
+                        city = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!aGuess);
+            
+        }
+    }
+}
---
+++ LoopExamples/LoopExamples/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 2;
+
+            /////
+            //You could skip the DO WHILE loop and just set the "number" variable to anything but 2
+            /////
+
+            /////
+            //These two are the same
+            /////
+            //while (isGuessed == false)
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62, try again!");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29, try again!");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55, try again!");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("You guessed 2, well done!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, you're wrong.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
