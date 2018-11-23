930cb86ae9019581463b6f914f740a3f061703d6
damsalem <dani@theamsalems.com>
2018-11-23 00:46:24 -0800

    Building loop programs

---
+++ LoopPrograms/LoopPrograms/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////
+            /////Create an array of strings
+            ///////
+            //string[] stringNames = { "David", "Teri", "Juliet", "Megan", "Dani", };
+
+            ////Display all strings in the array
+            //Console.WriteLine("This is the 1st step, creating an array of names, written as strings.");
+            //stringNames.ToList().ForEach(Console.WriteLine);
+
+            ////Create a new variable from user input
+            //Console.WriteLine("Now, please write your own name and hit [Enter] to replace all of the items on the list.");
+            //string userNames = Console.ReadLine();
+            
+            ////Update all strings in the array with the user input
+            //for (int i = 0; i < stringNames.Length; i++)
+            //{
+            //    stringNames[i] = userNames;
+            //}
+            
+            //Console.WriteLine("Now hit [Enter] to display the updated array");
+            //Console.ReadLine();
+
+            ////Display the new strings in the array
+            //for (int j = 0; j < stringNames.Length; j++)
+            //{
+            //    Console.WriteLine(stringNames[j]);
+            //}
+
+            //Console.ReadLine();
+
+
+            ///////
+            /////Create an infinite loop
+            ///////
+            ////Console.WriteLine("This is an infinite loop");
+            ////int k = 0;
+
+            ////while (k == 0)
+            ////{
+            ////    Console.WriteLine(userNames[0]);
+            ////}
+
+
+            ///////
+            /////Fix the infinite loop so it will execute
+            ///////
+            //Console.WriteLine("This is the 2nd step, an ending loop that writes a string 7 times." +
+            //    "\nPress [Enter] to start");
+            //Console.ReadLine();
+
+            //for (int l = 0; l < 7; l++)
+            //{
+            //    Console.WriteLine("Fee, Fi, Fo, Fum");
+            //}
+            //Console.ReadLine();
+
+
+            ////////
+            /////Create a loop where the comparison used to determine whether to continue iterating the loop is a /</ operator.
+            ///////
+            //Console.WriteLine("This is the 3rd step, a loop using a < operator." +
+            //    "\nPress [Enter] to start.");
+            //Console.ReadLine();
+
+            //for (int m = 0; m < 3; m++)
+            //{
+            //    Console.WriteLine("Eenie, Meenie, Miney, Mo");
+            //}
+            //Console.ReadLine();
+
+
+            ///////
+            /////Create a loop where the comparison used to determine whether to continue iterating the loop is a /</= operator.
+            ///////
+            //Console.WriteLine("This is the 4th step, a loop using a <= operator" +
+            //    "\nPress [Enter] to start.");
+            //Console.ReadLine();
+
+            //for (int n = 0; n <= 5; n++)
+            //{
+            //    Console.WriteLine("Rumplestiltskin");
+            //}
+            //Console.ReadLine();
+
+
+            /////
+            ///Create a list of strings where each item is unique. 
+            ///Display the list of strings.
+            ///Ask the user to select text to search within the list.
+            ///Create a loop that iterates through the list and then displays the index that contains the matching text.
+            /////
+            
+        }
+    }
+}
