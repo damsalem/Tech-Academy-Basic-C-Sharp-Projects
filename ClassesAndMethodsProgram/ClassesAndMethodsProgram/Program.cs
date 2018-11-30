dea85b67a5969437b4256989e53843abc2bf6f88
damsalem <dani@theamsalems.com>
2018-11-29 17:48:36 -0800

    Created a simple math classes and methods program

---
+++ ClassesAndMethodsProgram/ClassesAndMethodsProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Addition method
+            Console.WriteLine("Enter some number you want to add 10 to:");
+            int x = Int32.Parse(Console.ReadLine());
+
+            SomeMath justMath = new SomeMath();
+            int sum = justMath.simpleAddition(x);
+
+            Console.WriteLine(sum);
+
+            //Subtraction method
+            Console.WriteLine("Now enter some number you want to subtract 1 from:");
+            int y = Int32.Parse(Console.ReadLine());
+            
+            int difference = justMath.simpleSubtraction(y);
+
+            Console.WriteLine(difference);
+
+            //Multiplication method
+            Console.WriteLine("Finally, enter some number you want to multiply 2 with:");
+            int z = Int32.Parse(Console.ReadLine());
+
+            int product = justMath.simpleMultiplication(z);
+
+            Console.WriteLine(product);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassesAndMethodsProgram/ClassesAndMethodsProgram/SomeMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsProgram
+{
+    public class SomeMath
+    {
+        public int simpleAddition(int x)
+        {
+            int answer = x + 10;
+            return answer;
+        }
+
+        public int simpleSubtraction(int x)
+        {
+            int answer = x - 1;
+            return answer;
+        }
+
+        public int simpleMultiplication(int x)
+        {
+            int answer = x * 2;
+            return answer;
+        }
+    }
+}
