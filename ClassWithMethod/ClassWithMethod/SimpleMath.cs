c17d7359d10154435a4b3a3cfc18d005d16681e7
damsalem <dani@theamsalems.com>
2018-12-03 15:23:31 -0800

    Created 2 classes with void, overload and static

---
+++ ClassWithMethod/ClassWithMethod/SimpleMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWithMethod
+{
+    public class SimpleMath
+    {
+        //public int justDoIt(int userNum)
+        //{
+        //    int answer = userNum + 10;
+        //    return answer;
+        //}
+
+        public void simpleMethod(int userNum1)
+        {
+            int difference = userNum1 / 2;
+            Console.WriteLine(difference);
+        }
+
+        public void simpleMethod(int userNum2, out int answer1, out int answer2)
+        {
+            answer1 = userNum2 + 10;
+            answer2 = userNum2 * 2;
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+        }
+    }
+}
