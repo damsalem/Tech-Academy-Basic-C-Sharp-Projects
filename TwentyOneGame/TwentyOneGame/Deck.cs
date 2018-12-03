3beddbf880cdd2e8df5e2b705200afaead88d393
damsalem <dani@theamsalems.com>
2018-12-03 12:47:51 -0800

    Updated to include a class method and cleaner main class

--- TwentyOneGame/TwentyOneGame/Deck.cs
+++ TwentyOneGame/TwentyOneGame/Deck.cs
@@ -8,6 +8,7 @@ namespace TwentyOneGame
 {
     public class Deck
     {
+        //This is the constructor
         public Deck()
         {
             Cards = new List<Card>();
@@ -31,6 +32,30 @@ namespace TwentyOneGame
             }
 
         }
+
+        //This is the property
         public List<Card> Cards { get; set; }
+
+        //This is the method
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                /////
+                ///Instead of writing deck.Cards, this method was moved within the class
+                ///Then we could write this.Cards or just skip "this" entirely
+                ///
+                while (this.Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                this.Cards = TempList;
+            }
+        }
     }
 }
