16ab1d12bff389d72842e718ddbe66907cb693ac
damsalem <dani@theamsalems.com>
2018-12-03 12:06:23 -0800

    Created a simple math program that accepts optional arguments

---
+++ OptionalParameterClass/OptionalParameterClass/OptionalParameter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterClass
+{
+    public class OptionalParameter
+    {
+        //Create an optional argument/parameter by declaring its default value
+        public int singleOrDoubleParam(int requiredInt, int optionalInt = 0)
+        {
+            int answer = (requiredInt + optionalInt) * 2;
+            return answer;
+        }
+    }
+}
---
+++ OptionalParameterClass/OptionalParameterClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OptionalParameter twoNums = new OptionalParameter();
+
+            Console.WriteLine("I'm going to combine two numbers you enter, then multiply them by 2." + 
+                "\nPlease enter your first number");
+            int num1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now enter your second number, this is optional.");
+            string inputString = Console.ReadLine();
+
+            //Tries to parse "inputString" and if no value, twoNums.singleOrDoubleParam plugs in a "0"
+            Int32.TryParse(inputString, out int num2);
+            
+            int answer = twoNums.singleOrDoubleParam(requiredInt: num1, optionalInt: num2);
+            
+            Console.WriteLine("(" + num1 + " + " + num2 + ")" + " X 2 = " + answer);
+            Console.ReadLine();
+
+
+        }
+    }
+}
