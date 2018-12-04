921c0f292f620f30e0dd238c31acb524340d543e
damsalem <dani@theamsalems.com>
2018-12-03 17:47:11 -0800

    Created a simple program with inheritance and super classes

---
+++ SuperClassInheritance/SuperClassInheritance/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperClassInheritance
+{
+    public class Person
+    {
+        //Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void sayname()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+    }
+
+}
---
+++ SuperClassInheritance/SuperClassInheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperClassInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee dude = new Employee();
+            dude.FirstName = "Sample";
+            dude.LastName = "Student";
+            dude.sayname();
+            Console.ReadLine();
+        }
+    }
+}
