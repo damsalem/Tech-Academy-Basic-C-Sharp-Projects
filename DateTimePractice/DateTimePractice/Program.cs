a4b4160191f8f4ba3b1c41556c18851060a5c92b
damsalem <dani@theamsalems.com>
2018-12-10 21:56:54 -0800

    Letting users set an exact time in the future

--- DateTimePractice/DateTimePractice/Program.cs
+++ DateTimePractice/DateTimePractice/Program.cs
@@ -10,6 +10,11 @@ namespace DateTimePractice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The current date and time is: " + DateTime.Now);
+            Console.WriteLine("\nPlease enter a number of hours you want it to be in the future.");
+            int futureHour = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("In {0} hours it will be: {1}", futureHour, DateTime.Now.AddHours(futureHour));
+            Console.ReadLine();
         }
     }
 }
