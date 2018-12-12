b437d40981d248918e7834f5cfb6f71f9b03f691
damsalem <dani@theamsalems.com>
2018-12-11 17:46:24 -0800

    Built simple year birth year calculator with try catch and error handlings

---
+++ ExceptionsWithTryCatch/ExceptionsWithTryCatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsWithTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, please enter a number for your age");
+            int userAge = 0;
+            bool validAge = false;
+            try
+            {
+                validAge = Int32.TryParse(Console.ReadLine(), out userAge);
+                if (userAge <= 0)
+                {
+                    throw new NumberException();
+                }
+                int thisYear = DateTime.Now.Year;
+                int birthYear = thisYear - userAge;
+                Console.WriteLine("Because you are {0} years old, " +
+                    "it looks like you were born in {1}.", userAge, birthYear);
+                Console.ReadLine();
+            }
+            catch (NumberException)
+            {
+                Console.WriteLine("Hey, you didn't enter a number above 0, I quit!");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry, an error occurred. " +
+                    "Please contact your system administrator.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
