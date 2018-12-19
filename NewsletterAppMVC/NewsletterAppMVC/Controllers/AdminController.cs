8eecd9a5493e6934138bfc70aef3113330963263
Dani Amsalem <drummingdan@gmail.com>
2018-12-18 17:22:36 -0800

    Added another controller for admin

---
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsletterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities1 db = new NewsletterEntities1())
+            {
+                var signups = db.SignUps;
+                var signupVms = new List<SignUpVm>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignUpVm();
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms);
+            }
+            return View();
+        }
+    }
+}
