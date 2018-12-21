18223a91f3ad4fc0c7a541d6224d2e737546d373
Dani Amsalem <drummingdan@gmail.com>
2018-12-20 22:29:55 -0800

    Practicing Entity Framekwork Code First on a dB of Tech Academy students

---
+++ EntityFramekworkCodeFirst/EntityFramekworkCodeFirst/DAL/SchoolContext.cs
@@ -0,0 +1,26 @@
+﻿using EntityFramekworkCodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace EntityFramekworkCodeFirst.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+            ///The connection string will be passed in later through the web.config file
+            ///If it wasn't specified, the default name is the class name or "SchoolContext"
+        }
+
+        ///DbSet is an Entity Set. 
+        ///And an entity is an entry or a row in a table.
+        ///So a DbSet is a table.
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
