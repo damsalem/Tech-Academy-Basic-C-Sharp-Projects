a3f745f31b69238dc75fd1855ac444d029fe7a1b
Dani Amsalem <drummingdan@gmail.com>
2019-01-07 17:03:55 -0800

    Practicing technical interview items

---
+++ TechnicalInterviewQs/TechnicalInterviewQs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalInterviewQs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] oddNums = new int[5];
+            foreach (int num in nums)
+            {
+                if (isOdd(num))
+                {
+                    oddNums.
+                }
+            }
+            int sum = oddNums.Sum();
+            Console.WriteLine(sum);
+        }
+        public static bool isOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+    }
+}
