20d83ac22267f7df0a2b9472fb1430e637c3c6df
Dani Amsalem <drummingdan@gmail.com>
2019-01-07 20:13:00 -0800

    Completed several technical interview questions

--- TechnicalInterviewQs/TechnicalInterviewQs/Program.cs
+++ TechnicalInterviewQs/TechnicalInterviewQs/Program.cs
@@ -8,23 +8,49 @@ namespace TechnicalInterviewQs
 {
     class Program
     {
+        ///
+        ///Given an array of integers, write a method to total the odd numbers.
+        ///
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
         static void Main(string[] args)
         {
             int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             int[] oddNums = new int[5];
+            int i = 0;
             foreach (int num in nums)
             {
-                if (isOdd(num))
+                if (IsOdd(num))
                 {
-                    oddNums.
+                    oddNums.SetValue(num, i);
+                    i++;
                 }
             }
+            Console.WriteLine("The oddNums array = ");
+            foreach (int num in oddNums)
+            { Console.Write(num + ", "); }
+            Console.WriteLine();
             int sum = oddNums.Sum();
-            Console.WriteLine(sum);
-        }
-        public static bool isOdd(int value)
-        {
-            return value % 2 != 0;
+            Console.WriteLine("The sum of the oddNums array = " + sum);
+            Console.ReadLine();
+
+            ///Given an array of integers, write a method to sum the elements in the array,
+            ///knowing that some of the elements may be very large integers
+            ///
+            int[] bigNums = new int[] { 100, 1000, 10000, 100000, 10000000, 900000000, 900000000 };
+            //I noticed here that though MVS gives me an error if I make any of those ints larger
+            //I don't get an error when summing them up
+            int bigSum = bigNums.Sum();
+            Console.WriteLine(bigSum);
+            Console.ReadLine();
+
+            ///Given a string, reverse it.
+            ///
+            string aName = "Navar Adavan";
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
